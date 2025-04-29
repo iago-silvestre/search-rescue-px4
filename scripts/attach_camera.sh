5f8d10a2a71fe941b7cd7eeb9374c1a05651a0f1
iago silvestre <123979619+iago-silvestre@users.noreply.github.com>
2025-04-28 22:35:02 -0300

    Update attach_camera.sh

--- scripts/attach_camera.sh
+++ scripts/attach_camera.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 sleep 10
-rosservice call /link_attacher_node/attach "model_name_1: 'iris0'
-link_name_1: 'base_link'
-model_name_2: 'uav0_camera'
-link_name_2: 'camera_link'"
+rosservice call /link_attacher_node/attach "model_name_1: 'uav0_camera'
+link_name_1: 'camera_link'
+model_name_2: 'iris0'
+link_name_2: 'base_link'"
